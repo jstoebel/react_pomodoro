cc6cb2b4f97556e6575874827a6f18cdce4d31d6
Jacob Stoebel <jstoebel@gmail.com>
2019-01-11 09:43:28 -0500

    model test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    before(:each) do
+      @task = Task.new
+      @task.valid?
+    end
+    %i[name description].each do |column|
+      it "requires #{column}" do
+        expect(@task.errors.messages[column]).to eq(['can\'t be blank'])
+      end
+    end
+  end
 end
